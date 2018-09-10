998cb98b5ae9d259a8f125030b1910e9cd7dc7f8
vosel <yvosel@gmail.com>
2018-09-10 14:20:37 +0700

    [+] added simple script for html source analysyis
    Images links extractor

---
+++ extract_images_links_from_html_dump/core.js
@@ -0,0 +1,51 @@
+
+function backTrackAndExtractUrl(indexInsideUrl, lineString)
+{
+	var startIndex = indexInsideUrl;
+	var endIndex = indexInsideUrl;
+	var charIsPartOfUrl = function (character) {
+		return ((character != '\"') && (character != '\'') && (character != ' ')&& (character != ','));
+	};
+
+	do {
+		--startIndex;
+	} while ((startIndex >= 0) && charIsPartOfUrl(lineString.charAt(startIndex-1)));
+	do {
+		++endIndex;
+	} while ((endIndex < lineString.length-1) && charIsPartOfUrl(lineString.charAt(endIndex)));
+
+	return lineString.substr(startIndex, endIndex - startIndex);
+}
+
+function indexOfAny(lineToProcess, arrayOfStringsToSearch, startSearchIndex) {
+	var resultToReturn = null;
+	for (var i = 0; i < arrayOfStringsToSearch.length; ++i) {
+		var entry = arrayOfStringsToSearch[i];
+		var resultIndex = lineToProcess.indexOf(entry, startSearchIndex);
+		if (resultIndex > 0) {
+			if ((resultToReturn == null) || (resultToReturn.index > resultIndex)) {
+				resultToReturn = { matchedString: entry, index: resultIndex };
+			}
+		}
+	}
+	return resultToReturn;
+}
+
+// Note: the 'outputObject', should be used as a outlet for the results. This object should have newUrl() function for processing them.
+function MyLinesProcessor(outputObject)
+{
+	this.output = outputObject;
+	this.process = function (lineToProcess) {
+		var index = 0;
+		while ((index <= lineToProcess.length) && (index >= 0)) {
+			var searchResult = indexOfAny(lineToProcess, [".jpg", ".mp4"], index + 1);
+			if (searchResult != null) {
+				var url = backTrackAndExtractUrl(searchResult.index, lineToProcess);
+				index = searchResult.index;
+				this.output.newUrl(url);
+			} else {
+				return;
+			}
+		}
+	}
+}
